c4e6ffd553cf1cf219113c0b4758f10a288b4378
Jordyn <133882087+Jordynns@users.noreply.github.com>
2024-03-03 18:39:52 +0000

    Update dayspassed_calculator - Redo.py

--- dayspassed_calculator - Redo.py
+++ dayspassed_calculator - Redo.py
@@ -1,50 +1,50 @@
 # Days passed calculation
-# Jordyn
+# Jordyn Chalmers
 # Computer Programming Class - 16/02/2024
-
-# Imports the sys module
-import sys
 
 # Array list for total months and the days
 daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# Function to calculate if input year is a leapyear by checking if year is divisible by 4 but not 100 or unless it is divisible by 400. If these conditions are met returns True else returns False
+# Function to calculate if input year is a leapyear by checking if year is divisible by 4 but not 100 or unless it is divisible by 400 using the modulos operator. If these conditions are met returns True else returns False
 def leapYear(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     return False
 
-# Function that takes the 4 arguments/functions "day, month, year, daysInMonth" 
+# Function that takes the 4 arguments/variables "day, month, year, daysInMonth" 
 def passed(day, month, year, daysInMonth):
-    # Date validation to check if month is within 1-12 and if days are within 1-daysInMonth array list (If date is invalid print to log and exit script)
+    # Date validation to check if month is within 1-12 and if days are within 1-daysInMonth array list (If date is invalid print to log and return "None" to "total" variable which continues the while loop for the user to enter a valid date)
     if month < 1 or month > 12 or day < 1 or day > daysInMonth[month - 1]:
         print("Invalid date!\nPlease try again.")
-        userInput()
-
+        return None
     # Variable that takes sum of the days passed upto the user specified date.
     # sum() totals the days in the months
-    # + day adds the days in current month
+    # + day adds the days in current month along with -1 to day to account for the current day
     # [:month-1] subtracts the current month so it doesn't total the current month during the sum calculation
-    total = sum(daysInMonth[:month-1]) + day
+    total = sum(daysInMonth[:month-1]) + day -1
 
-    # If user entered month is greater than 2 (February) and leapYear function is True adds + 1 day to the total to account for the extra day in February 
-    if month > 2 and leapYear(year):
-        total += 1
-    # Returns calulated total days passed to "total" variable
+    # Check if leapYear is True and then checks if month is greater than 2 if not return total else so +1 to total to account for the extra day in February and return total
+    if leapYear(year) == True:
+        if month > 2:
+            total += 1
     return total
-
-# User input function to determin the dates and leapyear status
+# User input function with simple while loop to determin the dates and leapyear status
 def userInput():
-    day = int(input("Enter your day: "))
-    month = int(input("Enter your month (1/12): "))
-    year = int(input("Enter your year: "))
+    while True:
+        day = int(input("Enter your day: "))
+        month = int(input("Enter your month (1/12): "))
+        year = int(input("Enter your year: "))
 
-    # "passed" function is called with user input arguments/functions"day, month, year, daysInMonth" then total is calculated then the total days passed is printed to log along with if it's a leap year or not
-    total = passed(day, month, year, daysInMonth)
-    if leapYear(year):
-        print("Total days passed since the 1st of January: ", total,"\n",year, "is leap year!")
-    else:
-        print("Total days passed since the 1st of January: ", total,"\n",year, "is not a leap year!")
+        # "passed" function is called with user input arguments/functions"day, month, year, daysInMonth" 
+        # if "total" does not equal "None" runs leapyear check then breaks the while loop ending the script
+        total = passed(day, month, year, daysInMonth)
+        if total != None:
+            if leapYear(year):
+                print("Total days passed since the 1st of January: ", total,"\n",year, "is leap year!")
+                break
+            else:
+                print("Total days passed since the 1st of January: ", total,"\n",year, "is not a leap year!")
+                break
 
 # Calls userInput function
 userInput()
